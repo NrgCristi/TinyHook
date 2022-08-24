45693aa09bff4ecca295c240cb067d4c5d8202fd
NrgCristi <97525096+NrgCristi@users.noreply.github.com>
2022-08-24 12:29:16 +0300

    Add files via upload

---
+++ TinyHook.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "TinyHook.h" //" " signifies that it is a local library
+using namespace TinyHook;
+int TinyHook()
+{
+	TinyHook();
+	return 0;
+}
---
+++ TinyHook.h
@@ -0,0 +1,9 @@
+#ifdef TINYHOOK_H_
+#define TINYHOOK_H_
+namespace TinyHook {
+	void TinyHook()
+	{
+		std::cout << "Welcome to TinyHook The open source c++ library" << std::endl;
+	}
+}
+#endif
